f4abc0c09cd5b36489cfc567646abb4a1e7710a5
Alex Delchev <a.r.delchev@outlook.com>
2023-02-27 10:35:11 +0200

    Define RandomNumberGenerator interface

---
+++ src/main/java/org/example/random/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package org.example.random;
+
+/**
+ * Interface for random number generators.
+ */
+public interface RandomNumberGenerator {
+
+    /**
+     * Returns a randomly generated {@code int} value
+     *
+     * @return a random {@code int} value
+     */
+    int nextNum();
+}
